2113003ece5d397f61ed88fa33fe579589d87e4b
Arraytionary <31273055+Arraytionary@users.noreply.github.com>
2020-05-17 18:25:18 +0700

    splitting from vr

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{   
+    public ActionProcesser aP;
+    public string text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.GetComponent<Note>().text = text;
+        aP.OnTriggerEnter2D(collision);
+        //Debug.Log(text);
+        //Destroy(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        aP.OnTriggerExit2D(collision);
+    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    aP.OnCollisionEnter2D(collision);
+    //}
+}
---
+++ Assets/Scripts/MainValue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainValue : Singleton<MainValue>
+{
+    public IDictionary<string, Vector3> CameraPostion = new Dictionary<string, Vector3>()
+    {
+        { "mainGame", new Vector3(0,0,-10)}
+    };
+    public IDictionary<string, float> FloatValue = new Dictionary<string, float>()
+    {
+        { "left", 0f},
+        {"right", 0f }
+    };
+    public string selectedMenu = "all song";
+}
